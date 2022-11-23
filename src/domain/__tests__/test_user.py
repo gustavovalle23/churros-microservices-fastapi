e8acbfa645265a95d4e73f435ba76bb981d15283
gustavovalle23 <gustavo.vallerp@hotmail.com>
2022-11-23 20:19:13 -0300

    feat: updating gitignore to ignore pycharm settings

--- src/domain/__tests__/test_user.py
+++ src/domain/__tests__/test_user.py
@@ -14,5 +14,4 @@ def test_should_raise_error_when_email_is_empty():
 def test_should_raise_error_when_email_is_an_invalid_format():
     with pytest.raises(ValueError) as err:
         User("123", "Test", "invalid_email@", "password", True)
-
     assert err.value.args[0] == ['email: Should be a valid e-mail!']
