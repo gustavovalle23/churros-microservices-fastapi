bbcb166921cf6fd27f518d050ffe87b226588f37
gustavovalle23 <gustavo.vallerp@hotmail.com>
2022-12-31 19:14:25 -0300

    black run

--- src/user/domain/errors.py
+++ src/user/domain/errors.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 class EmailAlreadyRegisteredError(Exception):
     def __init__(self, email: str) -> None:
         super().__init__(f'Email "{email}" already registered')
