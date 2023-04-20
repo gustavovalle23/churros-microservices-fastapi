8c411bbfc0d4dbcbe3dc8b269995ebd88c139540
Gustavo Valle <gustavo.vallerp@hotmail.com>
2023-04-19 21:18:59 -0300

    :sparkles: feat(user): change User class to a functional approach - factory pattern

--- src/user/domain/entities.py
+++ src/user/domain/entities.py
@@ -2,37 +2,61 @@
 from typing import Optional
 from datetime import datetime
 from dataclasses import dataclass
+from collections import namedtuple
 
 from src.__seedwork.domain.entities import Entity
 from src.user.domain.value_objects import Address
 from src.user.domain.errors import NotificationError
 
 
-@dataclass(frozen=True)
-class User(Entity):
-    name: str
-    email: str
-    password: str
-    active: bool
-    points: int = 0
-    address: Optional[Address] = None
-    created_at: datetime = datetime.now()
-    updated_at: Optional[datetime] = None
+# @dataclass(frozen=True)
+# class User(Entity):
+#     name: str
+#     email: str
+#     password: str
+#     active: bool
+#     points: int = 0
+#     address: Optional[Address] = None
+#     created_at: datetime = datetime.now()
+#     updated_at: Optional[datetime] = None
 
-    def __post_init__(self):
-        self.validate()
+#     def __post_init__(self):
+#         self.validate()
 
-    def activate(self):
-        self.__setattr__("active", True)
+#     def activate(self):
+#         self.__setattr__("active", True)
 
-    def increase_points(self, points: int):
-        self.__setattr__("points", self.points + points)
+#     def increase_points(self, points: int):
+#         self.__setattr__("points", self.points + points)
 
-    def validate(self):
-        from src.user.domain.factories import UserValidatorFactory
+#     def validate(self):
+#         from src.user.domain.factories import UserValidatorFactory
 
-        validator = UserValidatorFactory.create()
-        validator.validate(self)
+#         validator = UserValidatorFactory.create()
+#         validator.validate(self)
 
-        if validator.has_errors():
-            raise NotificationError(validator.errors)
+#         if validator.has_errors():
+#             raise NotificationError(validator.errors)
+
+
+User = namedtuple('User', ['name', 'email', 'password', 'active', 'points', 'address', 'created_at', 'updated_at'])
+
+def create_user(name: str, email: str, password: str, active: bool, points: int = 0,
+                address: Optional[Address] = None, created_at: datetime = datetime.now(),
+                updated_at: Optional[datetime] = None) -> User:
+
+    from src.user.domain.factories import UserValidatorFactory
+    
+    user = User(name, email, password, active, points, address, created_at, updated_at)
+
+    validator = UserValidatorFactory.create()
+    validator.validate(user)
+
+    if validator.has_errors():
+        raise NotificationError(validator.errors)
+
+
+    user.activate = lambda: user._replace(active=True)
+    user.increase_points = lambda points: user._replace(points=user.points + points)
+
+    return user
--- src/user/domain/factories.py
+++ src/user/domain/factories.py
@@ -1,7 +1,7 @@
 from typing import Any
 
 from src.user.domain.validators import UserValidator
-from src.user.domain.entities import User
+from src.user.domain.entities import create_user, User
 
 
 class UserValidatorFactory:
@@ -11,15 +11,24 @@ class UserValidatorFactory:
 
 
 class UserFactory:
+    from src.core.database.models import UserModel
     @staticmethod
-    def create(user: Any) -> User:
-
-        return User(
-            user.id,
+    def create(user: UserModel) -> User:
+        return create_user(
             user.name,
             user.email,
             user.password,
-            user.active,
-            user.created_at,
-            user.updated_at,
+            active=user.active,
+            created_at=user.created_at,
+            updated_at=user.updated_at,
         )
+
+        # return User(
+        #     user.id,
+        #     user.name,
+        #     user.email,
+        #     user.password,
+        #     user.active,
+        #     user.created_at,
+        #     user.updated_at,
+        # )
